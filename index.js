a9ade379a0916c0f42b647dfe6829498b84d8233
Himanshu Rajauria <hrajauria1997@gmail.com>
2022-06-15 23:01:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1 = Math.floor(Math.random () * 6) + 1;
 var randomNumber2 = Math.floor(Math.random () * 6) + 1;
-var randomDice1 = "Dice" + randomNumber1 + ".png";
-var randomDice2 = "Dice" + randomNumber2 + ".png";
+var randomDice1 = "dice" + randomNumber1 + ".png";
+var randomDice2 = "dice" + randomNumber2 + ".png";
 var randomImageSource1 = "./images/" + randomDice1;
 var randomImageSource2 = "./images/" + randomDice2;
 document.querySelector (".img1").setAttribute ("src", randomImageSource1);
