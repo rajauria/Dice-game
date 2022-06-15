43b6d88efc7f3a595014a8fdc545d7f545ddcbce
Himanshu Rajauria <hrajauria1997@gmail.com>
2022-06-15 22:43:00 +0530

    Update path for images

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var randomNumber1 = Math.floor(Math.random () * 6) + 1;
 var randomNumber2 = Math.floor(Math.random () * 6) + 1;
 var randomDice1 = "Dice" + randomNumber1 + ".png";
 var randomDice2 = "Dice" + randomNumber2 + ".png";
-var randomImageSource1 = "Images/" + randomDice1;
-var randomImageSource2 = "Images/" + randomDice2;
+var randomImageSource1 = "images/" + randomDice1;
+var randomImageSource2 = "images/" + randomDice2;
 document.querySelector (".img1").setAttribute ("src", randomImageSource1);
 document.querySelector (".img2").setAttribute ("src", randomImageSource2);
 if (randomDice1>randomDice2){
